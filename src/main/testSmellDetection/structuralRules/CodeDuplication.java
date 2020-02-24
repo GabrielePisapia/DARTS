e4cbecefb678831f8be51d7bb597691a19956fe8
GabrielePisapia <50744224+GabrielePisapia@users.noreply.github.com>
2020-02-24 12:08:48 +0100

    Aggiunte detection per codeduplication,indirect testing e lazy test

---
+++ src/main/testSmellDetection/structuralRules/CodeDuplication.java
@@ -0,0 +1,17 @@
+package main.testSmellDetection.structuralRules;
+
+import java.io.File;
+
+public abstract class CodeDuplication {
+
+    public boolean isCodeDuplication(ClassBean pClassBean, String testsPath) throws IOException {
+        File result = new File(testsPath + "/clusters/post_cluster_vdb_100_2_allg_1.0_50");
+        if (result.exists()) {
+            String content = FileUtils.readFileToString(result);
+            String classPath = pClassBean.getBelongingPackage().replace(".", "/") + "/" + pClassBean.getName() + ".java";
+            return content.contains(classPath);
+        }
+        return false;
+    }
+
+}
---
+++ src/main/testSmellDetection/structuralRules/IndirectTesting.java
@@ -0,0 +1,19 @@
+package main.testSmellDetection.structuralRules;
+
+import java.util.List;
+
+public abstract class IndirectTesting {
+
+    public int  isIndirectTesting(MethodBean pMethod) {
+        int indirectTesting=0;
+        List<String> productionClassMethods=productionClass.getMethods().stream().map(x -> x.getName()).collect(Collectors.toList());
+        List<String> calls=pMethod.getMethodCalls().stream().map(x -> x.getName()).collect(Collectors.toList());
+        for(String s : calls) {
+            if (!productionClassMethods.contains(s)){
+                indirectTesting++;
+            }
+        }
+        return indirectTesting;
+    }
+
+}
